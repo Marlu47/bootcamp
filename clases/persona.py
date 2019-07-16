8cc2d389cd23ae3f67eb7dd79ab447ccb88c38b1
Marlucy Araujo <marluarasilg@gmail.com>
2019-07-16 15:44:54 -0400

    gitg

--- clases/persona.py
+++ clases/persona.py
@@ -1,11 +1,11 @@
-class Persona: 
+class Persona: #creamos la clase persona, seria como una receta o un plano
     edad = 0
 
-    def __init__ (self, un_nombre):
-        self.mi_nombre = un_nombre
+    def __init__ (self, un_nombre): #creamos los atributos (caracteristicas del objeto). metodo constructor
+        self.mi_nombre = un_nombre # self es para referirse al objeto mismo 
         print("Hola naci, me llamo", self.mi_nombre)
 
-    def cumple(self):
+    def cumple(self): #def es metodo cuando esta dentro de una clase(son las acciones que puede realizar el objeto)
         self.edad = self.edad + 1
 
 pepe = Persona("pepito")
@@ -13,5 +13,6 @@ pepa = Persona("pepita")
 print(pepe.edad)
 print(pepe.mi_nombre)
 
-pepe.cumple()
+for cumplir in range(6):
+    pepe.cumple()
 print(pepe.edad)

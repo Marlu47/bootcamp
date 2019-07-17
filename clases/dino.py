342b66463b7cddfc40081db7e2dd4385f1f30b61
Marlucy Araujo <marluarasilg@gmail.com>
2019-07-17 14:48:01 -0400

    creando a dino

---
+++ clases/dino.py
@@ -0,0 +1,43 @@
+#lo que uno pone en el metodo consy=tructor es especifico para ese objeto, 
+# no es para generalizar, por eso no lo ponemos en os atributos
+# se le puede definir las variables de una vez con el objetivo
+#none es una caja vacia donde podemos poner lo que queremos, puedo llenar o no llenar
+#al poner = en los argumentos. toma ese valor. ej patas=2, sin especificar en la lista 
+#una lista es para poner mas de un valor 
+class Dino:
+    ojos = 2
+    def __init__(self, un_nombre, un_color, canti_patas=4, un_genero=None):
+        self.nombre = un_nombre
+        self.color = un_color
+        self.patas = canti_patas
+        self.genero = un_genero
+        print("Naci")
+        
+    def saludar(self):
+        print("hola me llamo", self.nombre, "tengo", self.patas, "patas y soy", self.color)
+    
+    def cortar_pata(self, cantidad_de_patas_a_cortar=1):
+        self.patas = self.patas - cantidad_de_patas_a_cortar
+    
+    def decir_genero(self):
+        print("hola soy", self.nombre, "y me identifico como", self.genero)
+
+pepe = Dino ("kike", "verde", 4,"macho")
+pepe.saludar() #cuando son acciones (metodos) siempre termina con parentesis
+#cuando son atributos sin parentesis
+# print(pepe.nombre) 
+# print(pepe.color)
+# print(pepe.patas)
+# print(pepe.genero)
+#print("hola me llamo",pepe.nombre, "soy de color", pepe.color,"tengo", pepe.patas, "patas y me identifico como", pepe.genero)
+lilo = Dino ("lilodi", "rosa", 6, "hembra") #cada vez que creo un objeto tiene todas las caracteristicas y
+# puede realizar todas las acciones de la clase Dino
+lilo.saludar()
+
+rosa = Dino ("rosita", "purpura", 4, "hembra")
+rosa.saludar()
+rosa.cortar_pata()
+rosa.saludar()
+
+
+
--- clases/persona.py
+++ clases/persona.py
@@ -1,19 +1,60 @@
-class Persona: #creamos la clase persona, seria como una receta o un plano
-    edad = 0
-
-    def __init__ (self, un_nombre): #creamos los atributos (caracteristicas del objeto). metodo constructor
+class Persona: #creamos o definimos la clase persona, 
+    #se crea un objeto, nombre de la clase, instanciar: es crear un objeto unico 
+    #seria como una receta o un plano, para crear un objeto
+    #todo lo que esta en la clase es lo que va a crear el objeto, es el molde(clase)
+    edad = 0 #es un atributo de clase o propiedad, serian las caracteristicas 
+    nacionalidad = " " #son variables que estan dentro de una clase, los atributos.
+#metodos son las acciones que podemos realizar con los objetos
+    def __init__ (self, un_nombre): #creamos los atributos (caracteristicas del objeto). 
+        #metodo constructor, que construye el objeto
         self.mi_nombre = un_nombre # self es para referirse al objeto mismo 
-        print("Hola naci, me llamo", self.mi_nombre)
+        print("Hola naci, me llamo", self.mi_nombre) 
+        #los metodos son funciones dentro de una clase
 
     def cumple(self): #def es metodo cuando esta dentro de una clase(son las acciones que puede realizar el objeto)
-        self.edad = self.edad + 1
+        self.edad = self.edad + 1 #que aumenta la propiedad edad en 1
 
-pepe = Persona("pepito")
+    def asignar_edad(self, una_edad): #asignar edad es un metodo que recibe un argumento 
+        #y asigna a la propiedad  edad del objeto 
+        self.edad = una_edad
+    
+    def nac(self, una_nacio):
+        self.nacionalidad = una_nacio
+
+
+    
+
+pepe = Persona("pepito") #pepe es un objeto de la clase persona 
 pepa = Persona("pepita")
+mujer = Persona("Marlu")
+print(mujer.edad)
+print("soy ",mujer.mi_nombre," y tengo", mujer.edad, "y mi nacionalidad es", mujer.nacionalidad)
 print(pepe.edad)
 print(pepe.mi_nombre)
+mujer.nac("paraguaya")
+print("hola soy", mujer.mi_nombre, "y mi nacionalidad es", mujer.nacionalidad )
 
-for cumplir in range(6):
-    pepe.cumple()
-print(pepe.edad)
+# for cumplir in range(6):
+#     pepe.cumple()
+# print(pepe.edad)
+
+#ejer: agregar un metodo en la clase persona que asigne nacionalidad
+#y otro metodo saludar que imprima "hola soy nombre y mi nacionalidad es"
+
+# hacer una clase que se llama vehiculo que tenga tres atributos 
+# y uno que sea cantidad_ruedas, y un metodo que sea definir _tipo 
+# que me diga si es "bicicleta, triciclo,auto" de acuerdo a ala cantidad de ruedas
+
+class Vehiculo:
+    ruedas = 0
+    color = ""
+    modelo = ""
+    
+    def __init__(self, cantidad_ruedas): #se pueden pasar todos los atributos (variables)
+        self.ruedas = cantidad_ruedas
+    
+    def col(self, un_color):
+        self.color = un_color
+
+
 
